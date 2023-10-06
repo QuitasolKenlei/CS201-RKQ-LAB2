6886ab34562cc6da8c389b71b98635feae6073e4
QuitasolKenlei <146052692+QuitasolKenlei@users.noreply.github.com>
2023-10-06 11:48:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+//Act2
+let firstFavNum = 9;
+let secondFavNum = 2;
+
+console.log(firstFavNum > secondFavNum);
+console.log(firstFavNum < secondFavNum);
+console.log(firstFavNum >= secondFavNum);
+console.log(firstFavNum <= secondFavNum);
+console.log(firstFavNum === secondFavNum);
+console.log(firstFavNum == secondFavNum);
+console.log(firstFavNum !== secondFavNum);
+console.log(firstFavNum != secondFavNum);
